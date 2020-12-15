ab2b6d769388c5a720af3926a7e830056efbb840
Bruno Fruchard <perso@brunofruchard.com>
2020-12-15 16:45:26 +0100

    clean: removed renderer file

--- software/GUI (Electron)/renderer.js
+++
@@ -1,5 +0,0 @@
-// // load and save files
-// const fs = require('fs');
-// // manage dialog windows to save/load files
-// const { dialog } = require("electron").remote;
-// const { ipcRenderer } = require('electron');
