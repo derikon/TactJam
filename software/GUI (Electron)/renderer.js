dd5854401cca1e180b87d5e3417e33ef20b5764a
Bruno Fruchard <perso@brunofruchard.com>
2020-12-15 16:16:11 +0100

    feat(electron-app): first draft of the Electron app

---
+++ software/GUI (Electron)/renderer.js
@@ -0,0 +1,5 @@
+// // load and save files
+// const fs = require('fs');
+// // manage dialog windows to save/load files
+// const { dialog } = require("electron").remote;
+// const { ipcRenderer } = require('electron');
